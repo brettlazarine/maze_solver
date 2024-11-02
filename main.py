2f1c46a87dae70060da746d7862024d9fdd17609
brettlazarine <jblazarine@gmail.com>
2024-11-02 17:53:24 -0500

    complete step 4

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from graphics import Window
+from graphics import *
 
 def main():
     win = Window(800, 600)
+
+    point1 = Point(0, 0)
+    point2 = Point(500, 500)
+
+    line = Line(point1, point2)
+    
+    win.draw_line(line, "red")
+
     win.wait_for_close()
 
 if __name__ == "__main__":
