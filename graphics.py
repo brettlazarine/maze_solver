114e40b00fcf03583bc4e49532adac0a67577a17
brettlazarine <jblazarine@gmail.com>
2024-11-03 09:38:39 -0600

    complete step 5

--- graphics.py
+++ graphics.py
@@ -23,7 +23,7 @@ class Window:
     def close(self):
         self.__running = False
 
-    def draw_line(self, line, fill_color):
+    def draw_line(self, line, fill_color="black"):
         line.draw(self.__canvas, fill_color)
 
 class Point:
@@ -36,5 +36,32 @@ class Line:
         self.point1 = point1
         self.point2 = point2
 
-    def draw(self, canvas, fill_color):
+    def draw(self, canvas, fill_color="black"):
         canvas.create_line(self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill=fill_color, width=2)
+
+class Cell:
+    def __init__(self, win):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+        self._win = win
+
+    def draw(self, x1, y1, x2, y2):
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        if self.has_left_wall:
+            self._win.draw_line(
+                Line(Point(self._x1, self._y1), Point(self._x1, self._y2)))
+        if self.has_right_wall:
+            self._win.draw_line(Line(Point(self._x2, self._y1), Point(self._x2, self._y2)))
+        if self.has_top_wall:
+            self._win.draw_line(Line(Point(self._x1, self._y1), Point(self._x2, self._y1)))
+        if self.has_bottom_wall:
+            self._win.draw_line(Line(Point(self._x1, self._y2), Point(self._x2, self._y2)))
--- main.py
+++ main.py
@@ -10,6 +10,15 @@ def main():
     
     win.draw_line(line, "red")
 
+    cell1 = Cell(win)
+    cell1.draw(100, 500, 200, 200)
+    cell2 = Cell(win)
+    cell2.draw(200, 100, 300, 700)
+    cell3 = Cell(win)
+    cell3.draw(300, 100, 400, 200)
+    cell4 = Cell(win)
+    cell4.draw(400, 600, 500, 200)
+
     win.wait_for_close()
 
 if __name__ == "__main__":
